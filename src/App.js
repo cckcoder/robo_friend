0053576490db6ec3749db06ac50735bb170ca730
codewiz <c.chaiwat.ch@gmail.com>
2020-02-01 00:32:38 +0700

    Complete search, add background

--- src/App.js
+++ src/App.js
@@ -12,16 +12,20 @@ class App extends React.Component {
     }
   }
 
-  onSearchChange(event) {
-    console.log(event)
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value })
   }
 
   render() {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+    })
+
     return (
       <div className='tc'>
         <h1>RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots={this.state.robots}/>
+        <CardList robots={filteredRobots}/>
       </div>
     )
   }
