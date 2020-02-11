b0ec3a95721eceadb5fb346a79f334d006e42d7d
codewiz <c.chaiwat.ch@gmail.com>
2020-02-11 07:44:49 +0700

    refresh redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider, connect } from 'react-redux'
 import './index.css';
 import App from './container/App'
 import * as serviceWorker from './serviceWorker';
