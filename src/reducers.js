d0fa88377204b99d23d894af3e078057cd97b4f1
codewiz <c.chaiwat.ch@gmail.com>
2020-02-13 07:53:31 +0700

    reducer async action

--- src/reducers.js
+++ src/reducers.js
@@ -1,4 +1,9 @@
-import { CHANGE_SEARCH_FIELD } from './constants'
+import {
+  CHANGE_SEARCH_FIELD
+  ,REQUEST_ROBOTS_PENDING
+  ,REQUEST_ROBOTS_SUCCESS
+  ,REQUEST_ROBOTS_FAILED
+} from './constants.js'
 
 const intialState = {
   searchField: ''
@@ -10,5 +15,17 @@ export const searchRobots = (state=intialState, action={}) => {
       return Object.assign({}, state, { searchField: action.payload })
     default:
       return state
+  }
+}
+
+export const requestRobots = (state=intialState, action={}) => {
+  switch(action.type) {
+    case REQUEST_ROBOTS_PENDING:
+      return Object.assign({}, state, { isPending: true})
+    case REQUEST_ROBOTS_SUCCESS:
+      return Object.assign({}, state, {
+        robots: action.type.payload
+        ,isPending: false
+      })
   }
 }
